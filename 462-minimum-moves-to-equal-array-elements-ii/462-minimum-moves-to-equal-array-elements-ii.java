41706304292e9ef33012ced33f0ecc200ecc3735
Goldikumar143 <108024520+Goldikumar143@users.noreply.github.com>
2022-06-30 18:49:23 +0530

    Time: 4 ms (90.02%), Space: 43.1 MB (88.44%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
@@ -2,6 +2,7 @@ class Solution {
     public int minMoves2(int[] nums) {
         int count=0;
         Arrays.sort(nums);
+        
         int mid=nums.length/2;
         for(int i=0;i<nums.length;i++){
             if(nums.length==1){
@@ -9,9 +10,9 @@ class Solution {
             }
            else{
             
-               int sum=Math.abs(nums[mid]-nums[i]);
-              System.out.println(sum);
-            count+=sum;
+              count+=Math.abs(nums[mid]-nums[i]);
+              
+            
            }     
           
         }
