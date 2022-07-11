5387b09decdc41bd93aeaee9b832db6ecded612a
Goldikumar143 <108024520+Goldikumar143@users.noreply.github.com>
2022-07-11 18:39:49 +0530

    Time: 1 ms (86.19%), Space: 64.2 MB (61.36%) - LeetHub

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -1,52 +1,22 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-      // List<Integer> list=new ArrayList<Integer>(nums.length);
-       
-       
-       
-        
-//         int len3=nums.length;
-       
-       
-       
-        
-// //         for(int i=0;i<len;i++){
-// //          list.add(nums[l+i]);
-           
-// //             System.out.print(list);
-        
-// //         }
-// //          for(int i=0;i<l;i++){
-// //           list.add(nums[i]);
-// //           //  System.out.print(list);
-        
-// //         }
-        
-//         for(int i=0;i<nums.length;i++){
-
-//         int j=(i+k)%len3;
-//              int len=k%nums.length;
-//             // System.out.print(len);
-       
-          
-//           System.out.print(j);
-//             list.add( j,  nums[i]);
-//         }
-        
-//          for(int i=0;i<list.size();i++){
-//               nums[i]=list.get(i);
-        
-//         }
-        
-        int n=nums.length;
-        int res[]=new int[n];
-        for(int i=0;i<n;i++){
-            res[(i+k)%n]=nums[i];
-        }
-         for(int i=0;i<n;i++){
-             nums[i]=res[i];
- }
+      int n = nums.length;
+        k %= n;
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+    
+    public void reverse(int[] a, int low, int high) {
+        while(low < high) {
+            int temp = a[low];
+            a[low] = a[high];
+            a[high] = temp;
+            low++;
+            high--;
        
       
     }
+}
+    
 }
